a484dd0abf14c2eeb3f1bb17e997bb65843f29d4
Dorin <dorin@stormbits.com>
2016-03-17 18:55:20 +0100

    Adding new plots

--- consumer_control.py
+++ consumer_control.py
@@ -1,5 +1,6 @@
 import config
 import datetime
+import json
 import os
 import time
 import matplotlib.pyplot as plt
@@ -109,16 +110,10 @@ def clear_all_reports():
 
 def show_report():
     median_amount = 1000
-    timestamp_dict = {}
     today = datetime.datetime.now()
-    # daca = False
 
     # iterate in all files
     for i in range(0, len(config.consumer_ips)):
-        # if not daca:
-        #     daca = True
-        # else:
-        #     continue
 
         for worker in range(0, config.worker_numbers[config.consumer_ips[i]]):
 
@@ -126,6 +121,12 @@ def show_report():
             if not os.path.isfile("report_worker%d_%d.out" % (i, worker)):
                 continue
 
+            # if there is a json dump created, then skip this worker
+            if os.path.isfile("report_worker%d_%d.json" % (i, worker)):
+                continue
+
+            # otherwise parse the file and create a dictionary which will be dumped in a new file
+            timestamp_list = []
             with open("report_worker%d_%d.out" % (i, worker)) as f:
                 # iterate in all lines
                 for line in f:
@@ -142,44 +143,83 @@ def show_report():
                         # for simplicity the timestamp has only hours, minutes and seconds
                         dt = dt.replace(year=today.year, month=today.month, day=today.day)
 
-                        # if there are identical timestamps, keep adding one microsecond
-                        while dt in timestamp_dict:
-                            dt = dt.replace(microsecond=dt.microsecond + 1)
-
                         # get the epoch time and add the microseconds
                         epoch = time.mktime(dt.timetuple()) + dt.microsecond / 1000000.0
 
-                        timestamp_dict[epoch] = float(result.strip())
+                        timestamp_list.append([epoch, float(result.strip())])
+                        # timestamp_dict[epoch] = float(result.strip())
                     except Exception as e:
                         print e
                         pass
 
+            # dump the dictionary in a new file
+            with open("report_worker%d_%d.json" % (i, worker), 'w') as f:
+                f.write(json.dumps(timestamp_list))
+
+    # iterate through all json dumps again and plot the graphs
+    plt.figure(1)
+    timestamp_dict = {}
+    for i in range(0, len(config.consumer_ips)):
+        for worker in range(0, config.worker_numbers[config.consumer_ips[i]]):
+            with open("report_worker%d_%d.json" % (i, worker)) as f:
+                plm = f.read()
+                temp_list = json.loads(plm)
+
+                for key, value in temp_list:
+                    while key in timestamp_dict:
+                        key += 0.000001
+
+                    timestamp_dict[key] = value
+
+                list_1 = [x[0] for x in temp_list]
+                list_1 = [key - list_1[0] for key in list_1]
+                list_1 = get_median_list(list_1, median_amount)
+
+                list_2 = [x[1] for x in temp_list]
+                list_2 = [key for key in list_2]
+                list_2 = get_median_list(list_2, median_amount)
+
+                plt.plot(list_1, list_2, linestyle="-", linewidth=2.0, marker=None, label='Worker%d_%d' % (i, worker))
+    plt.legend(loc="upper right", ncol=2, shadow=True, title="Legend", fancybox=True)
+
+    # get all timestamps from the dictionary
     timestamp_list = [key for key in timestamp_dict]
+
+    # sort them
     timestamp_list.sort()
-    timestamp_median_list = get_result_median_list(timestamp_list, median_amount)
+
+    # get the sampled list
+    timestamp_median_list = get_median_list(timestamp_list, median_amount)
+
+    # remove the smallest timestamp from all others. This is to ensure it starts from 0
     timestamp_median_list = [key - timestamp_median_list[0] for key in timestamp_median_list]
 
+    # get all the results from the dictionary, based on the sorted timestamp list
     result_list = [timestamp_dict[timestamp] for timestamp in timestamp_list]
-    result_median_list = get_result_median_list(result_list, median_amount)
 
-    # with open("timestamps.out", 'w') as f:
-    #    f.write(json.dumps(timestamp_list))
+    # get the sampled list
+    result_median_list = get_median_list(result_list, median_amount)
 
-    # with open("results.out", 'w') as f:
-    #    f.write(json.dumps(result_list))
+    # make 3 subplots
+    plt.figure(2)
+    plt.subplot(4, 1, 1)
 
-    fig, ax = plt.subplots()
-    # ax.set_xlim([min(timestamp_median_list), max(timestamp_median_list)])
-    # ax.set_ylim([0,max(result_median_list)])
-    ax.plot(timestamp_median_list, result_median_list, linestyle="-", linewidth=2.0, marker=None,
-            label='NoSQL response time')
+    # plot the response times
+    plt.plot(timestamp_median_list, result_median_list, linestyle="-",
+             linewidth=2.0, marker=None, label='NoSQL response time')
 
-    plt.xlabel('Run time [s]')
+    max_timestamp = max(timestamp_median_list)
+    max_result = max(result_median_list)
+
+    plt.axis((0, max_timestamp, 0, max_result))
+    plt.legend(loc="upper right", ncol=2, shadow=True, title="Legend", fancybox=True)
+
     plt.ylabel('Execution time [s]')
 
     for i in range(0, len(config.nosql_ips)):
 
         usage_list = []
+        memory_list = []
         # in case there is one missing, skip
         if not os.path.isfile("cpu_usage_nosql%s.out" % config.nosql_ips[i]):
             continue
@@ -189,12 +229,31 @@ def show_report():
             for line in f:
                 line_split = line.split(' ')
                 usage = line_split[-4]  # cpu usage is the fourth in vmstat
+                memory = line_split[4]
 
-                usage_list.append(float(usage)/100)
+                usage_list.append(float(usage))
+                memory_list.append(float(memory))
 
-        max_len = int(max(timestamp_median_list))
         max_len = len(usage_list)
-        ax.plot(range(0, max_len), usage_list[0: max_len], linestyle=':', label='CPU Usage %s' % config.nosql_ips[i])
+        plt.subplot(4, 1, 2)
+        plt.plot(range(0, max_len), usage_list[0: max_len], linestyle=':', label='CPU Usage %s' % config.nosql_ips[i])
+        axes = plt.gca()
+        axes.set_xlim([0, max_timestamp])
+        axes.set_ylim([0, max(usage_list)])
+
+        plt.legend(loc="upper right", ncol=2, shadow=True, title="Legend", fancybox=True)
+        plt.ylabel('Usage [%]')
+
+        max_len = len(memory_list)
+        plt.subplot(4, 1, 3)
+        plt.plot(range(0, max_len), memory_list[0: max_len], linestyle=':', label='Free memory %s'
+                                                                                  % config.nosql_ips[i])
+        axes = plt.gca()
+        axes.set_xlim([0, max_timestamp])
+        axes.set_ylim([0, max(memory_list)])
+
+        plt.legend(loc="upper right", ncol=2, shadow=True, title="Legend", fancybox=True)
+        plt.ylabel('Memory [KB]')
 
     for i in range(0, len(config.nosql_ips)):
 
@@ -207,18 +266,21 @@ def show_report():
             # iterate in all lines
             for line in f:
                 line_split = line.split('load average:')
-                line_split = line_split[1].split(',') # get only everything after load average
-                load = line_split[0]  # cpu usage is the fourth in vmstat
+                line_split = line_split[1].split(',')  # get only everything after load average
+                load = line_split[0]
 
-                load_list.append(float(load)/(100 * config.concurrency))
+                load_list.append(float(load))
 
-        max_len = int(max(timestamp_median_list))
         max_len = len(load_list)
-        ax.plot(range(0, max_len), load_list[0: max_len], linestyle='--', label='CPU Load %s' % config.nosql_ips[i])
+        plt.subplot(4, 1, 4)
+        plt.plot(range(0, max_len), load_list[0: max_len], linestyle='--', label='CPU Load %s' % config.nosql_ips[i])
+        axes = plt.gca()
+        axes.set_xlim([0, max_timestamp])
 
-    legend = ax.legend(loc='upper center', shadow=True)
-    frame = legend.get_frame()
-    frame.set_facecolor('0.90')
+    plt.legend(loc="upper right", ncol=2, shadow=True, title="Legend", fancybox=True)
+    plt.xlabel('Run time [s]')
+    plt.ylabel('Load')
+
     plt.show()
 
 
@@ -226,7 +288,7 @@ def get_sublist(split_list, amount):
     return [split_list[i:i+amount] for i in range(0, len(split_list), amount)]
 
 
-def get_result_median_list(results, amount):
+def get_median_list(results, amount):
 
     median_list = []
     for sublist in get_sublist(results, amount):
