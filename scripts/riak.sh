e01ca5b6de8a013911c3af863841b8582f6da61a
Dorin <dorin@stormbits.com>
2016-04-07 12:23:35 +0200

    Cleaning unused files

--- scripts/riak.sh
+++
@@ -1,7 +0,0 @@
-curl https://packagecloud.io/install/repositories/basho/riak/script.deb.sh | sudo bash
-sudo apt-get install riak=2.1.1-1
-
-sudo sed -i 's/127.0.0.1/192.168.33.15/g' /etc/riak/riak.conf
-
-riak stop
-riak start
