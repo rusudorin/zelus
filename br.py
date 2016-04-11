5685a95b8fbfa07b1342781a6cb314d0c0133d8a
Dorin <dorin@stormbits.com>
2016-04-11 17:24:30 +0200

    Removing handlers from Stormtrooper files folder

--- br.py
+++
@@ -1,8 +0,0 @@
-# broadcast to a certain Celery worker which rate to use
-
-from celery import Celery
-
-app = Celery('tasks', broker='amqp://guest@localhost//')
-
-app.control.broadcast('rate_limit', arguments={'task_name': 'tasks.add', 'rate_limit': '40/s'})
-
